1ba132017268fdf2d0a261f80415d16a3664eae9
Vinicius <vinigugelmin@hotmail.com>
2022-05-17 12:45:05 -0300

    Preparando revisão de código

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import reactDom from "react-dom";
-
-import App from "./componentes/App.jsx";
-
-import "./reset.css";
-import "./estilo.css";
-
-reactDom.render(<App />, document.querySelector(".root"));
